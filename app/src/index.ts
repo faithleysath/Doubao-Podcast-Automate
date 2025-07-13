23ab5420f7d1260354dfd41f610f3a244f7e198f
faithleysath <faithleysath@gmail.com>
2025-07-13 20:07:44 +0800

    test file upload endpoint

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,6 +1,15 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .post("/", 
+    () => "Hello Elysia", 
+    { 
+      body: t.Form({ 
+        type: t.UnionEnum(['url', 'article', 'file']), 
+        content: t.Union([t.String({ format: 'uri' }), t.String({ minLength: 500 }), t.File({ type: 'application/pdf', maxSize: '5m' })]) 
+      }) 
+    })
+    .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
